c2355075be4145ccb082e6286b82724a494c8133
Kaan <kaansahin3800@gmail.com>
2024-03-08 01:56:47 +0300

    _

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import { FaWindowClose } from "react-icons/fa";
+const App = () => {
+   
+  const [data, setData] = useState([
+    {
+      key : '',
+      value : ''
+    }
+  ])
+   
+ 
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className='nav'>
+      {
+        data.map((item, index) => (
+          <span>
+            <input type="text" value={item.key} onChange={((e) => {
+              setData(items => items.map((item, i) => {
+                if (index === i) {
+                  item.key = e.target.value
+                }
+                return item
+              }))
+            })}/>
+            <input type="text"  value={item.value}   onChange={((e) => {
+              setData(items => items.map((item, i) => {
+                if (index === i) {
+                  item.value = e.target.value
+                }
+                return item
+              }))
+            })}/>
+          {
+            data.length > 1 ? (
+              <h4 onClick={(() => {
+                setData(items => items.filter((e, key) => key !== index)) 
+               })}>
+               <FaWindowClose className='close_icon' />
+               </h4>
+            ) :
+            null
+          }
+          </span>
+        ))
+       }
+       <h3 onClick={() => setData((e) => [...e, {
+         key : '',
+         value : ''
+       }])}>
+        SUBMİT
+       </h3>
+      </nav>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
